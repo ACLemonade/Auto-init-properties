27c86d47866aa477ad4e65e07aac2d32876f18bd
Lemonade <842476697@qq.com>
2017-02-22 09:25:41 +0800

    Initial Commit

---
+++ Auto-init-Properties/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Auto-init-Properties
+//
+//  Created by Lemonade on 2017/2/22.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
